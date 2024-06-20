4e0608d32284910bffb2ea5639bf64bb1d16f902
gabriellm.dev <gabriellm.dev@gmail.com>
2024-06-20 16:18:05 -0300

    created project

---
+++ src/main/java/com/br/glm/mylibrary_literalura/MylibraryLiteraluraApplication.java
@@ -0,0 +1,13 @@
+package com.br.glm.mylibrary_literalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylibraryLiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylibraryLiteraluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/glm/mylibrary_literalura/MylibraryLiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.glm.mylibrary_literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylibraryLiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
