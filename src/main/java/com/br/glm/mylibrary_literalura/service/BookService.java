be8b50dd6a2047f89c3d868e5030673ec8edc729
gabriellm.dev <gabriellm.dev@gmail.com>
2024-06-21 15:24:59 -0300

    criado a httpresponse e utilizacao do client para fazer requisicao

--- src/main/java/com/br/glm/mylibrary_literalura/service/BookService.java
+++ src/main/java/com/br/glm/mylibrary_literalura/service/BookService.java
@@ -1,10 +1,16 @@
 package com.br.glm.mylibrary_literalura.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class BookService {
@@ -21,5 +27,23 @@ public class BookService {
                 .build();
     }
 
+    public CompletableFuture<HttpResponse<String>> sendRequest(HttpRequest request) {
+        return httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public JsonNode getBooks(String query) {
+        HttpRequest request = buildRequest(query);
+        CompletableFuture<HttpResponse<String>> responseFuture = sendRequest(request);
+
+        try {
+            HttpResponse<String> response = responseFuture.get();
+            String responseBody = response.body();
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.readTree(responseBody);
+        } catch (InterruptedException | ExecutionException | IOException e) {
+            e.printStackTrace();
+            return null;
+        }
 
 }
